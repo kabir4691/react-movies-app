e4a631d7793303573903643c5a2eb9b1648a0078
Kabir <kabir4691@gmail.com>
2020-07-05 04:44:49 +0530

    Add logo to header

--- src/common/header/Header.jsx
+++ src/common/header/Header.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import './Header.css'
 import Button from '@material-ui/core/Button'
+import logo from '../../assets/logo.svg'
 
 export default class extends Component {
   render() {
     return (
       <div className='app-header'>
+        <img src={logo} className='app-logo' alt='logo'/>
         <Button className='login-button' variant='contained' color='default'>
           Login
         </Button>
