4abe39ac64b213829c0ccb7920020e8fad462e9d
Kabir <kabir4691@gmail.com>
2020-07-05 15:06:07 +0530

    Add grid list for Released Movies on home page

--- src/screens/home/Home.jsx
+++ src/screens/home/Home.jsx
@@ -10,19 +10,23 @@ import GridListTile from '@material-ui/core/GridListTile'
 
 const styles = theme => ({
   root: {
-      flexGrow: 1,
-      backgroundColor: theme.palette.background.paper
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.paper
   },
   upcomingMoviesHeading: {
-      textAlign: 'center',
-      background: '#ff9999',
-      padding: '8px',
-      fontSize: '1rem'
+    textAlign: 'center',
+    background: '#ff9999',
+    padding: '8px',
+    fontSize: '1rem'
   },
   gridListUpcomingMovies: {
-      flexWrap: 'nowrap',
-      transform: 'translateZ(0)',
-      width: '100%'
+    flexWrap: 'nowrap',
+    transform: 'translateZ(0)',
+    width: '100%'
+  },
+  gridListMain: {
+    transform: 'translateZ(0)',
+    cursor: 'pointer'
   }
 });
 
@@ -43,6 +47,21 @@ class Home extends Component {
             </GridListTile>
           ))}
         </GridList>
+        <div className="flex-container">
+          <div className="left">
+            <GridList cellHeight={350} cols={4} className={classes.gridListMain}>
+              {moviesData.map(movie => (
+                <GridListTile className="released-movie-grid-item" key={"grid" + movie.id}>
+                  <img src={movie.poster_url} className="movie-poster" alt={movie.title} />
+                  <GridListTileBar title={movie.title} subtitle={<span>Release Date: {new Date(movie.release_date).toDateString()}</span>} />
+                </GridListTile>
+              ))}
+            </GridList>
+          </div>
+          <div className="right">
+
+          </div>
+        </div>
       </div>
     )
   }
