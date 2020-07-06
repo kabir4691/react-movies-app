3d7ba4872d738d44ff2b4402d73dd10122a2ee34
Kabir <kabir4691@gmail.com>
2020-07-06 06:59:23 +0530

    Add apply button to movies filter form on home page

--- src/screens/home/Home.jsx
+++ src/screens/home/Home.jsx
@@ -20,6 +20,7 @@ import MenuItem from '@material-ui/core/MenuItem'
 import Checkbox from '@material-ui/core/Checkbox'
 import ListItemText from '@material-ui/core/ListItemText'
 import TextField from '@material-ui/core/TextField'
+import Button from '@material-ui/core/Button'
 
 const styles = theme => ({
   root: {
@@ -163,6 +164,9 @@ class Home extends Component {
                       InputLabelProps={{ shrink: true }}
                     />
                   </FormControl>
+                  <br />
+                  <br />
+                  <Button variant='contained' color='primary'>Apply</Button>
                 </FormControl>
               </CardContent>
             </Card>
